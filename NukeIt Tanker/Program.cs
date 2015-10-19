35d077ba556cc589cddf32ce1e59aab877d5b380
Anuradha Sanjeewa Wickramarachchi <anuradhawick@gmail.com>
2015-10-19 23:53:33 +0530

    Message handler
    Building the chain of responsibilities

--- NukeIt Tanker/Program.cs
+++ NukeIt Tanker/Program.cs
@@ -14,9 +14,11 @@ namespace NukeIt_Tanker
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+            string s = "Anuradha";
+            Console.WriteLine(s[0]);
         }
     }
 }
